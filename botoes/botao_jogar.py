977430e202a6b14fc12231bf6edb8bacc25244c8
vitorueno <vitoruenootto@gmail.com>
2019-10-26 20:48:03 -0300

    template criado

---
+++ botoes/botao_jogar.py
@@ -0,0 +1,7 @@
+  
+import arcade
+from .botao import Botao
+
+class Botao_jogar(Botao):
+    def __init__(self,center_x,center_y):
+        super().__init__("img/botoes/botao_jogar.png",center_x=center_x,center_y=center_y,width=115,height=36,escala=1,acao_botao=1)
---
+++ jogo.py
@@ -0,0 +1,55 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Starting Template"
+
+#estados do jogo
+SAIR = -1
+MENU = 0
+PARTIDA = 1
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+
+    def setup(self):
+
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+
+    def on_update(self, delta_time):
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        pass
+
+
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
